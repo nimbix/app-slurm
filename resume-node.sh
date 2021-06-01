093c3a102937e5d6d630cf7d9bd7cdd8c6ce5b55
Kenneth Hill <ken.hill@nimbix.net>
2021-06-01 17:19:07 -0500

    Add gang scheduling
    Signed-off-by: Kenneth Hill <ken.hill@nimbix.net>

--- resume-node.sh
+++ resume-node.sh
@@ -5,30 +5,29 @@ queue=$(echo $1 | sed 's/jarvice-//g' | sed 's/[[]*[0-9].*//g')
 queue_config=$(cat /etc/slurm-llnl/partitions.json)
 slurm_config=$(cat /etc/slurm-llnl/slurm-configpath)
 # Check if range of nodes is specified
-for group in $(echo $1 | sed -r 's/(.*[a-zA-z]+)([0-9]+)$/[\2]/' \
+for group in $(echo $1 | sed -r 's/(.*[a-zA-Z]+)([0-9]+)$/[\2]/' \
     | awk -F'[][]' '{print $2}' | tr "," "\n"); do
     myRange=$group
-    if [ -z "$myRange" ]; then
-        myNumber=$(echo $group | grep -Eo '[0-9]+$')
-        myRange="$myNumber-$myNumber"
-    fi
     loopSeq=$(seq $(cut -d'-' -f1 <<<$myRange) $(cut -d'-' -f2 <<<$myRange))
-    for index in $loopSeq; do
-        nodeName="jarvice-$queue$index"
+    nodeCount=$(echo $loopSeq | wc -w)
+
+    indexStart=$(echo $loopSeq | awk '{print $1}')
 jobscript=$(cat << EOF
 if [ \$(which apt) ]; then
     SLURMDIR=/etc/slurm-llnl
     DEBIAN_FRONTEND=noninteractive sudo apt update && \
-    DEBIAN_FRONTEND=noninteractive sudo apt install -yq slurmd
+    DEBIAN_FRONTEND=noninteractive sudo apt install -yq slurmd # netcat
 elif [ \$(which yum) ]; then
     SLURMDIR=/etc/slurm
     cd $SLURM_YUMDIR
-    sudo yum install -y *
+    sudo yum install -y * # netcat
 else
     echo \$(cat /etc/issue) not supported
 fi
 sudo mkdir -p \$SLURMDIR
-worker_name=$nodeName
+node_rank=\$(cat /etc/JARVICE/nodes | grep -n \$(hostname) | sed -r 's/([0-9]+):.*$/\1/')
+node_rank=\$(( \$node_rank + $indexStart - 1 ))
+worker_name="jarvice-$queue\$node_rank"
 sudo cp -r ${slurm_config}/* \$SLURMDIR
 cat \$SLURMDIR/slurm-headnode | sudo tee --append /etc/hosts
 node=\$(hostname)
@@ -41,10 +40,44 @@ while true; do
         break
     fi
 done
+if [ "\$1" != "worker" ]; then
+    myHost=\$(hostname)
+    while IFS= read -r node; do
+        echo "Checking node: \$node"
+        if [ "\$node" != "\$myHost" ]; then
+#            while true; do
+#                sleep 5
+#                nc -zv \$node 22
+#                if [ \$? -eq 0 ]; then
+#                    break
+#                fi
+#            done
+            echo "Setting up node: \$node"
+            scp /opt/JARVICE/jobscript \$node:/tmp
+            (ssh \$node "nohup bash /tmp/jobscript worker &> /dev/null") &
+        fi
+    done < /etc/JARVICE/nodes
+    echo "test!!!! \$line"
+        node=\$(tail -n 1 /etc/JARVICE/nodes)
+        echo "Checking node: \$node"
+        if [ "\$node" != "\$myHost" ]; then
+#            while true; do
+#                sleep 5
+#                nc -zv \$node 22
+#                if [ \$? -eq 0 ]; then
+#                    break
+#                fi
+#            done
+            echo "Setting up node: \$node"
+            scp /opt/JARVICE/jobscript \$node:/tmp
+            (ssh \$node "nohup bash /tmp/jobscript worker &> /dev/null") &
+        fi
+fi
 sudo mkdir -p /var/run/munge && sudo chown munge:munge /var/run/munge
 sudo -u munge munged -f --key-file=\$SLURMDIR/munge.key
 sudo mkdir -p /var/spool/slurmd
 sudo mkdir -p /var/run/slurmd
+sudo mkdir -p /var/log/slurm
 sudo slurmd -b -D
 EOF
 )
@@ -59,7 +92,7 @@ jxe_job=$(cat << EOF
   },
   "machine": {
     "type": "$(echo $queue_config | jq -r .$queue.machine)",
-    "nodes": 1
+    "nodes": $nodeCount
   },
   "hpc": {
     "hpc_job_env_config": "",
@@ -83,44 +116,47 @@ jxe_job=$(cat << EOF
 }
 EOF
 )
-        rm $slurm_config/jxe-$nodeName $slurm_config/$nodeName
-        resp=$(echo $jxe_job | curl -X POST \
-            -H "Content-Type: application/json" \
-            --data-binary @- "$APIURL"jarvice/submit)
-        number=$(echo $resp | jq -r .number)
-        echo $number | sudo tee /etc/slurm-llnl/jxe-$nodeName
+    for nodeIndex in $loopSeq; do
+        myNodeName="jarvice-$queue$nodeIndex"
+        rm $slurm_config/jxe-$myNodeName $slurm_config/$myNodeName
+    done
+    resp=$(echo $jxe_job | curl -X POST \
+        -H "Content-Type: application/json" \
+        --data-binary @- "$APIURL"jarvice/submit)
+    number=$(echo $resp | jq -r .number)
+    while true; do
+        sleep 15
+        job_status=$(curl --data-urlencode "username=$APIUSER" \
+            --data-urlencode "apikey=$APIKEY" \
+            --data-urlencode "number=$number" \
+            "$APIURL""jarvice/status")
+        index=$( printf '%d' $number )
+        job_status=$(echo $job_status | jq -r .[\"$index\"].job_status)
+        echo $job_status
+        if [ "$job_status" = "PROCESSING STARTING" ]; then
+            echo "worker $number online"
+            break
+        fi
+    done
+    for nodeIndex in $loopSeq; do
+        myNodeName="jarvice-$queue$nodeIndex"
+        touch $slurm_config/jxe-$myNodeName
         while true; do
-            sleep 15
-            job_status=$(curl --data-urlencode "username=$APIUSER" \
-                --data-urlencode "apikey=$APIKEY" \
-                --data-urlencode "number=$number" \
-                "$APIURL""jarvice/status")
-            index=$( printf '%d' $number )
-            job_status=$(echo $job_status | jq -r .[\"$index\"].job_status)
-            echo $job_status
-            if [ "$job_status" = "PROCESSING STARTING" ]; then
-                echo "worker $number online"
-                break
-            fi
-        done
-        touch $slurm_config/jxe-$nodeName
-        while true; do
-            if [ -f "$slurm_config/$nodeName" ]; then
+            if [ -f "$slurm_config/$myNodeName" ]; then
                 echo "worker ready"
                 break
             fi
             sleep 5
         done
-        slurm_worker_config=$(cat $slurm_config/$nodeName)
-        worker_ip=$(echo $slurm_worker_config | awk '{print $1}')
-        echo "$nodeName.jarvice.slurm.  IN   A   $worker_ip" | sudo tee --append /root/slurm.db
-        soa_update=$(sudo awk 'NR==1{$6='"$(date +\"%Y%m%M%S\")"'; print}' /root/slurm.db)
-        # arecords=$(sudo cat /root/slurm.db | sed '/.*SOA.*/d')
-        sudo cat /root/slurm.db | sed "1s/.*/$soa_update/" | sudo tee /root/slurm.db
-        # echo $arecords | sudo tee --append /root/slurm.db
-        sleep 15
+        echo $number | sudo tee /etc/slurm-llnl/jxe-$myNodeName
+        echo $myNodeName | sudo tee --append /etc/slurm-llnl/$number
+        slurm_worker_config=$(cat $slurm_config/$myNodeName)
         echo $slurm_worker_config | sudo tee --append /etc/hosts
-        sudo scontrol update nodename=$nodeName nodeaddr=$worker_ip \
-            nodehostname=$nodeName
+        worker_ip=$(echo $slurm_worker_config | awk '{print $1}')
+        echo "$myNodeName.jarvice.slurm.  IN   A   $worker_ip" | sudo tee --append /root/slurm.db
+        soa_update=$(sudo awk 'NR==1{$6='"$(date +\"%Y%m%M%S\")"'; print}' /root/slurm.db)
+        sudo cat /root/slurm.db | sed "1s/.*/$soa_update/" | sudo tee /root/slurm.db
+        sudo scontrol update nodename=$myNodeName nodeaddr=$worker_ip \
+            nodehostname=$myNodeName
     done
 done
--- suspend-node.sh
+++ suspend-node.sh
@@ -5,21 +5,24 @@ queue=$(echo $1 | sed 's/jarvice-//g' | sed 's/[[]*[0-9].*//g')
 for group in $(echo $1 | sed -r 's/(.*[a-zA-z]+)([0-9]+)$/[\2]/' \
     | awk -F'[][]' '{print $2}' | tr "," "\n"); do
     myRange=$group
-    if [ -z "$myRange" ]; then
-        myNumber=$(echo $group | grep -Eo '[0-9]+$')
-        myRange="$myNumber-$myNumber"
-    fi
     loopSeq=$(seq $(cut -d'-' -f1 <<<$myRange) $(cut -d'-' -f2 <<<$myRange))
     for index in $loopSeq; do
         nodeName="jarvice-$queue$index"
-
         number=$(cat /etc/slurm-llnl/jxe-$nodeName)
-        sudo rm /etc/slurm-llnl/jxe-$nodeName
-        cat /etc/hosts | sed "/.*$nodeName/d" | sudo tee /etc/hosts
-        cat /root/slurm.db | sed "/.*$nodeName.*/d" | sudo tee /root/slurm.db
+        groupNames=$(cat /etc/slurm-llnl/$number)
+        for node in $groupNames; do
+            sudo scontrol update nodename="$node" state="down" reason="evict"
+        done
         curl --data-urlencode "username=$APIUSER" \
             --data-urlencode "apikey=$APIKEY" \
             --data-urlencode "number=$number" \
             "$APIURL""jarvice/shutdown"
+        for node in $groupNames; do
+            sudo rm -f /etc/slurm-llnl/jxe-$node
+            cat /etc/hosts | sed "/.*$node/d" | sudo tee /etc/hosts
+            cat /root/slurm.db | sed "/.*$node.*/d" | sudo tee /root/slurm.db
+            sudo scontrol update nodename="$node" state="resume"
+        done
+        rm /etc/slurm-llnl/$number
     done
 done
